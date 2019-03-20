1a470386e5ac3bf756c5a52373138fe4d33c8bdd
Pravin Pawar <Pravin.Pawar@INFOVISIONLABS.COM>
2019-03-20 13:15:32 +0530

    storybook for button and badge

--- src/stories/item.stories.js
+++ src/stories/item.stories.js
@@ -1,53 +1,52 @@
-import React from 'react';
-import {
-  storiesOf
-} from '@storybook/react';
-import {
-  action
-} from '@storybook/addon-actions';
-import {
-  withKnobs,
-  object
-} from '@storybook/addon-knobs/react';
-import Item from '../components/Item';
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { withKnobs, object } from "@storybook/addon-knobs/react";
+import Item from "../components/Item";
 
 export const item = {
   id: 1,
-  name: 'Apple iPhone Xs Max',
-  image: 'appleIphoneXS.png',
-  description: 'Facial Recognition, Fingerprint Scanner',
-  brand: 'Apple',
+  name: "Apple iPhone Xs Max",
+  image: "appleIphoneXS.png",
+  description: "Facial Recognition, Fingerprint Scanner",
+  brand: "Apple",
   price: 99000,
-  type: 'Mobile',
-}
-
+  type: "Mobile"
+};
 
 export const itemnoDiscription = {
   id: 1,
-  name: 'Apple',
-  image: 'appleIphoneXS.png',
-  description: '',
-  brand: 'Apple',
+  name: "Apple",
+  image: "appleIphoneXS.png",
+  description: "",
+  brand: "Apple",
   price: 900,
-  type: 'Mobile',
+  type: "Mobile"
 };
 
-storiesOf('items', module)
+storiesOf("items", module)
   .addDecorator(withKnobs)
-  .add('default', () => {
-    return <div className = "list-group" > < Item item = {
-      object('items', {
-        ...item
-      })
-    }
-    /></div > ;
+  .add("default", () => {
+    return (
+      <div className="list-group">
+        {" "}
+        <Item
+          item={object("items", {
+            ...item
+          })}
+        />
+      </div>
+    );
   })
-  .add('no Description', () => {
-    return <div className = "list-group" > < Item item = {
-      object('items', {
-        ...itemnoDiscription
-      })
-    }
-    /></div > 
+  .add("no Description", () => {
+    return (
+      <div className="list-group">
+        {" "}
+        <Item
+          item={object("items", {
+            ...itemnoDiscription
+          })}
+        />
+      </div>
+    );
   });
-  
