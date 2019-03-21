4f892c2cd59dba3063444d7807ae0e4651209526
Pravin Pawar <Pravin.Pawar@INFOVISIONLABS.COM>
2019-03-21 12:57:31 +0530

    css updates

--- src/components/products/Item.js
+++ src/components/products/Item.js
@@ -1,15 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Item = ( { item }) => {
+const Item = ({ item }) => {
   return (
-      <div>
-          <img src={item.image} className="img-thumbnail" alt={"img"} />
-          <h5 className="card-title" title={item.name}>{item.name}</h5>
-          <h6><i className="fa fa-usd" aria-hidden="true"></i> {item.price}</h6>
-      </div>
-  )
-}
+    <div>
+      <h6 className="priceTag">
+        {item.price} <i className="fa fa-inr" aria-hidden="true" />
+      </h6>
+      <img src={item.image} className="img-thumbnail" alt={"img"} />
+      <h5 className="card-title" title={item.name}>
+        {item.name}
+      </h5>
+    </div>
+  );
+};
 
 Item.propTypes = {
   item: PropTypes.any
